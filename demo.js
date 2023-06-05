aac164d5ac12e793154725df65368e0f9a7c8092
Henry Morgan <76470485+henatic@users.noreply.github.com>
2023-06-05 10:12:10 -0700

    Working example.

--- demo.js
+++ demo.js
@@ -1,16 +1,17 @@
 async function Data() {
     // Default options are marked with *
-    const response = await fetch("https://accounts.zoho.com/oauth/v2/auth?code=1000.dc1eefe0ae225ca04cbc7438cbd4a31d.7924eec6f2b3ad2a0563cfe95a819e45&grant_type=authorization_code&client_id=1000.LZAWBOTEYQ2MYNCVATLVEECK367TIB&client_secret=7d3e6fdbd93812879c39567fd7f450859330adf8b8", {
+    const response = await fetch("https://accounts.zoho.com/oauth/v2/token?code=1000.02ea29196c35ed07ce7b4581896afae9.0c586ce3fbf1d93ab4a72a469b561ef2&grant_type=authorization_code&client_id=1000.LZAWBOTEYQ2MYNCVATLVEECK367TIB&client_secret=7d3e6fdbd93812879c39567fd7f450859330adf8b8&redirect_uri=https://www.zylker.com/oauthgrant", {
       method: "POST", // *GET, POST, PUT, DELETE, etc.
       mode: "cors", // no-cors, *cors, same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
       credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        "orgId": "749689656",
-        "Authorization": "Zoho-oauthtoken 1000.dc1eefe0ae225ca04cbc7438cbd4a31d.7924eec6f2b3ad2a0563cfe95a819e45"
-      },
+      // headers: {
+      //   "orgId": "749689656",
+      //   "Authorization": "Zoho-oauthtoken 1000.a8bb31bcbee40b64afe002a17e5d0a6f.7c3885f16eda2b152bbf79f8171012d7"
+      // },
       redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer"// no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      referrerPolicy: "no-referrer",// no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify({"code":"1000.02ea29196c35ed07ce7b4581896afae9.0c586ce3fbf1d93ab4a72a469b561ef2", "grant_type":"authorization_code", "client_id":"1000.LZAWBOTEYQ2MYNCVATLVEECK367TIB","client_secret":"7d3e6fdbd93812879c39567fd7f450859330adf8b8", "redirect_uri":"https://www.zylker.com/oauthgrant"})
     });
     return response.json(); // parses JSON response into native JavaScript objects
   }
@@ -24,16 +25,35 @@ async function Data() {
       credentials: "same-origin", // include, *same-origin, omit
       headers: {
         "orgId": "749689656",
-        "Authorization": "Zoho-oauthtoken 1000.dc1eefe0ae225ca04cbc7438cbd4a31d.7924eec6f2b3ad2a0563cfe95a819e45"
+        "Authorization":"Zoho-oauthtoken 1000.4856165e9e16f647783ca1456d549764.3df54f77e920e476bcc00f4567515010",
+        "Content-Type": "application/json"
       },
       redirect: "follow", // manual, *follow, error
       referrerPolicy: "no-referrer",// no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-      body: JSON.stringify('{"subject":"Test","departmentId":"749689656","description":"This ticket was created using Node.js!  No further action is needed."}')
+      body: JSON.stringify({"subject":"Test", "departmentId":"601361000000006907","description":"This ticket was created using Node.js!  No further action is needed.", "contactId":"601361000017707258"})
     });
     return response.json(); // parses JSON response into native JavaScript objects
   }
 
-Data().then(data => {
+  async function Department() {
+    // Default options are marked with *
+    const response = await fetch("https://desk.zoho.com/api/v1/departments", {
+      method: "GET", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "orgId": "749689656",
+        "Authorization":"Zoho-oauthtoken 1000.024df6df59ea882543032fa1e575378e.70e95c9aeefca6d6c5ce91f28f32b65e",
+        "Content-Type": "application/json"
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer"// no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    });
+    return response.json(); // parses JSON response into native JavaScript objects
+  }
+
+Ticket().then(data => {
   console.log(data);
   
 }).catch(err => console.log(err));
